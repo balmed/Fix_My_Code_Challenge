8632e3c807364aa18f846da9bcf381800345526d
balmed <iwymed@gamil.com>
2024-01-24 13:57:51 -0800

    task 1

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 /*
     Print a square with the character #
-    
     The size of the square must be the first argument 
     of the program.
 */
@@ -14,7 +13,7 @@ if (process.argv.length <= 2) {
     process.exit(1)
 }
 
-size = parseInt(process.argv[2], 16)
+size = parseInt(process.argv[2], 10)
 
 for (let i = 0 ; i < size ; i ++) {
     for (let j = 0 ; j < size ; j ++) {
@@ -22,3 +21,4 @@ for (let i = 0 ; i < size ; i ++) {
     }
     process.stdout.write("\n");
 }
+
